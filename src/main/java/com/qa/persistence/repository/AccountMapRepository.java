8cba6338dfc663f2412457df942994b31d9fabe7
phileasFoggy22 <phileasFoggy@gmail.com>
2019-06-04 18:20:37 +0100

    first 5 tests done

--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -1,41 +1,82 @@
 package com.qa.persistence.repository;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
 
-public class AccountMapRepository implements AccountRepository{
-	
-	Map<Integer, Account> accountMap = new HashMap<Integer, Account>();
-	
-	//You must provide concrete implementation for each of these methods
-	//do not change the method signature
-	//THINK - if the parameter is a String, or the return type is a String
-	//How can I convert to a String from an Object?
-	//What utility methods do I have available?
-	
-	//You must complete this section using TDD
-	//You can use the suggested tests or build your own.
+public class AccountMapRepository implements AccountRepository {
+
+	private Map<Integer, Account> accountMap;
+
+	// You must provide concrete implementation for each of these methods
+	// do not change the method signature
+	// THINK - if the parameter is a String, or the return type is a String
+	// How can I convert to a String from an Object?
+	// What utility methods do I have available?
+
+	// You must complete this section using TDD
+	// You can use the suggested tests or build your own.
 
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+
+		JSONUtil util = new JSONUtil();
+		return util.getJSONForObject(accountMap);
+
+	}
+
+	public AccountMapRepository(Map<Integer, Account> accountMap) {
+		super();
+		this.accountMap = accountMap;
+	}
+
+	public Map<Integer, Account> getAccountMap() {
+		return this.accountMap;
+	}
+
+	public void setAccountMap(Map<Integer, Account> accountMap) {
+		this.accountMap = accountMap;
 	}
 
 	public String createAccount(String account) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Account util = new JSONUtil().getObjectForJSON(account, Account.class);
+			this.getAccountMap().put(util.getId(), util);
+			return "Added Successfully";
+
+		} catch (Exception e) {
+			return e.getMessage();
+		}
 	}
 
 	public String deleteAccount(int accountNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			if (this.getAccountMap().containsKey(accountNumber)) {
+				this.getAccountMap().remove(accountNumber);
+				// System.out.println("Removed Successfully");
+				return "Removed Successfully";
+			} else {
+				// System.out.println("Not Removed Successfully");
+
+				return "Record does not exist";
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return e.getMessage();
+		}
+
 	}
 
 	public String updateAccount(int accountNumber, String account) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Account util = new JSONUtil().getObjectForJSON(account, Account.class);
+			this.getAccountMap().put(accountNumber, util);
+			return "Updated Successfully";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+
 	}
 
 }
--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -1,75 +1,98 @@
 package com.qa.MapTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Assert;
-import org.junit.Before;
+import java.util.HashMap;
+
 import org.junit.Test;
 
 import com.qa.persistence.domain.Account;
 import com.qa.persistence.repository.AccountMapRepository;
+import com.qa.util.JSONUtil;
 
 public class AccountServiceTest {
 
-	@Before
-	public void setup() {
-	
-	}
-	
+	private AccountMapRepository amr = new AccountMapRepository(new HashMap<Integer, Account>());
+
 	@Test
 	public void addAccountTest() {
-		fail("TODO");	
-	}
-	
-	@Test
-	public void add2AccountsTest() {
-		fail("TODO");	
+
+		Account acc1 = new Account(1, "1", "Matt", "Hunt");
+		String util = new JSONUtil().getJSONForObject(acc1);
+		assertEquals("Added Successfully", amr.createAccount(util));
+
 	}
 
 	@Test
-	public void removeAccountTest() {
-		fail("TODO");	
+	public void add2AccountsTest() {
+		Account acc2 = new Account(2, "2", "Matty", "Huntsworth");
+		Account acc1 = new Account(3, "3", "Matthew", "Hunter");
+		amr.createAccount(new JSONUtil().getJSONForObject(acc2));
+		String util = new JSONUtil().getJSONForObject(acc1);
+		assertEquals("Added Successfully", amr.createAccount(util));
+
 	}
-	
+
+	@Test
+	public void removeAccountTest() { //
+		Account acc2 = new Account(2, "2", "Matthew", "Hunter");
+		amr.createAccount(new JSONUtil().getJSONForObject(acc2));
+		assertEquals("Removed Successfully", amr.deleteAccount(2));
+	}
+
 	@Test
 	public void remove2AccountsTest() {
-		fail("TODO");	
+		Account acc2 = new Account(2, "2", "Matty", "Huntsworth");
+		amr.createAccount(new JSONUtil().getJSONForObject(acc2));
+		Account acc1 = new Account(3, "3", "Matthew", "Hunter");
+		amr.createAccount(new JSONUtil().getJSONForObject(acc1));
+		assertEquals("Removed Successfully", amr.deleteAccount(2));
+		assertEquals("Removed Successfully", amr.deleteAccount(3));
 	}
-	
+
 	@Test
 	public void remove2AccountTestAnd1ThatDoesntExist() {
-		fail("TODO");	
+		Account acc1 = new Account(1, "1", "Matt", "Hunt");
+		Account acc2 = new Account(2, "2", "Matty", "Huntsworth");
+		Account acc3 = new Account(3, "3", "Matthew", "Hunter");
+		amr.createAccount(new JSONUtil().getJSONForObject(acc3));
+		amr.createAccount(new JSONUtil().getJSONForObject(acc2));
+		amr.createAccount(new JSONUtil().getJSONForObject(acc1));
+		assertEquals("Removed Successfully", amr.deleteAccount(2));
+		assertEquals("Removed Successfully", amr.deleteAccount(3));
+		assertEquals("Record does not exist", amr.deleteAccount(4));
+		// assertEquals("Record does not exist", amr.deleteAccount(2));
 	}
-	
+
 	@Test
 	public void jsonStringToAccountConversionTest() {
 		// testing JSONUtil
-		fail("TODO");	
+		fail("TODO");
 	}
-
 
 	@Test
 	public void accountConversionToJSONTest() {
-		//testing JSONUtil
-		fail("TODO");	
+		// testing JSONUtil
+		fail("TODO");
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality
+		fail("TODO");
 	}
-	
+
 	@Test
 	public void getCountForFirstNamesInAccountWhenOne() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality f
+		fail("TODO");
 	}
 
 	@Test
 	public void getCountForFirstNamesInAccountWhenTwo() {
-		//For a later piece of functionality
-		fail("TODO");	
+		// For a later piece of functionality
+		fail("TODO");
 	}
 
 }
